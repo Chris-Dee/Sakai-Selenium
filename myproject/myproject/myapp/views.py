aacf25312cced9cf687b65ff89e12354eda83c45
Chris-Dee <deechristopher0@gmail.com>
2016-05-05 07:13:47 +0000

    got output to display, now run test as subprocess so that test exits properly

--- myproject/myproject/myapp/views.py
+++ myproject/myproject/myapp/views.py
@@ -4,6 +4,9 @@ import sys
 import json
 import imp
 import time
+import csv
+import subprocess
+from StringIO import StringIO
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -59,26 +62,63 @@ def list(request):
 def script(request):
 
     if request.method == 'GET':
+
         title = request.GET['title']
+        url = request.GET['url']
         current_file = Document.objects.get(title=title).docfile
-        target = open('.' + current_file.url, 'r')
+        
+	#Write URL to CSV
+        responseCSV = HttpResponse(content_type='text/csv')
+        responseCSV['Content-Disposition'] = 'attachment; filename="defaultname.csv"'
+        writer = csv.writer(responseCSV)
+        writer.writerow(['url', url])
+	
         timestring = str(int(time.time()))
         stderr_temp = sys.stderr
-        target = open("." + current_file.url, 'r')      
-        log_file = open("selenium_log" + timestring, 'w+')
-        sys.stderr = log_file
-        test_import = imp.load_source('test_import', '.' + current_file.url)
-        #print test_import.__dict__.keys()
-        test_import.unittest.main(module=test_import, argv=sys.argv[:1], exit=False)
-        sys.stderr = stderr_temp
-        log_file.close()
-        read_file = open("selenium_log" + timestring, 'r')
-        test_output = read_file.read()
-        print read_file.read()
+	stdout_temp = sys.stdout
+        print sys.stderr
+        curr_path = os.path.dirname(os.path.abspath(__file__))
+        log_filename = curr_path + "/logs/"+title+"selenium_log" + timestring + ".log"
+        log_filename=log_filename.replace(" ", "_")
+	print log_filename
+	#log_string = StringIO()
+        if not os.path.exists(os.path.dirname(log_filename)):
+                print "yay"
+                try:
+                    os.makedirs(os.path.dirname(log_filename))
+		except OSError:
+                    raise Exception("creating directory failed")
+        log_file = open(log_filename, 'w+')
+
+	#print sys.stderr
+	#print sys.stdout
+        #sys.stderr = log_string
+	#sys.stdout = log_string
+
+	#test_import = imp.load_source('test_import', '.' + current_file.url)
+        subprocess.call(["python", os.getcwd() + current_file.url], stdout=log_file, stderr=log_file)	
+        #test_import.unittest.main(module=test_import, argv=sys.argv[:1], exit=False)
+	
+	#print sys.stderr
+	#print sys.stdout
+        #sys.stderr = stderr_temp
+	#sys.stdout = stdout_temp
+	#print sys.stderr
+	#print sys.stdout
+        #log_file.write(log_string.getvalue())
+#        log_file = open("selenium_log" + timestring, 'r')
+        
+	#test_output = log_string.getvalue()
+        log_file.close() 
+	log_file = open(log_filename, 'r')
+	test_output = log_file.read()
+        print log_file.read()
         response_data = {
-            'result': test_output 
+            'result': test_output
         }
+	print "right before"
         print test_output
+	log_file.close()
         return HttpResponse(json.dumps(response_data), content_type="application/json", status=200)
     
     if request.method == 'DELETE':
